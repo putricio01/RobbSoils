a0efb6e9362c1550538bbe3c2d29f8209c6d4c9e
ketimporta1 <patriciobc23@gmail.com>
2024-11-02 12:31:40 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
+  //output: "server",
+  //adapter: netlify()
+  output: "static"
 });
